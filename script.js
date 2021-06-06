ea9c86ebe6af9ee922ee961e641444439e6d8581
bisrat2020 <wow.bisrat11@gmail.com>
2021-06-06 14:52:04 -0400

    setting the geolocation API

--- script.js
+++ script.js
@@ -34,16 +34,16 @@ const loginPage = `
 <button id="loginBtn">Login</button></div>`;
 
 const animationPage =`<div id="anamtion">
-<h3 id="mylocation">Location</h3>
-<textarea name="" id="" cols="35" rows="15"></textarea><br><br>
+<span ><strong id="mylocation"></strong></span><br>
+<textarea name="" id="display" cols="35" rows="15"></textarea><br><br>
 <button id="refresh">Refresh Animation</button>
 <button id="logout">LogOut</button>
 </div>`
 
+const key = `kgwCfjylCzg0GhWKC2aaaMcLIc10aIQO`
+
 let mainDiv = document.getElementById("outlet");
      mainDiv.innerHTML = loginPage;
-    //  mainDiv.style.marginLeft = '40%'
-    //  mainDiv.style.marginTop = '20%';
 let loginDiv = document.getElementById('loginDiv');
 loginDiv.classList.add('area')
  
@@ -51,11 +51,66 @@ loginDiv.classList.add('area')
 
 
 let loginBtn = document.getElementById('loginBtn');
-loginBtn.addEventListener('click',function(){
+loginBtn.addEventListener('click',()=>{
     console.log('clicked')
     history.pushState("AnimationPage","?animation");
     mainDiv.innerHTML = animationPage;
+    userLocation()
 
      let animDiv = document.getElementById("anamtion")
       animDiv.classList.add('area')
+      
+      let despaly = document.getElementById('display')
+      let refreshBtn = document.getElementById('refresh');
+      let logoutBtn = document.getElementById('logout')
+    
+      refreshBtn.addEventListener('click',()=> {
+          console.log('refrashed');
+      })
 })
+let mylocation;
+let myLong,myLati;
+ function userLocation(){
+     navigator.geolocation.getCurrentPosition(
+         function(thisPosition){
+             myLong = thisPosition.coords.longitude;
+             myLati = thisPosition.coords.latitude;
+             console.log(myLong)
+             fetchLocation();
+         } 
+     )
+ }
+//  "http://open.mapquestapi.com/geocoding/v1/reverse?key=kgwCfjylCzg0GhWKC2aaaMcLIc10aIQO&location=38.987779499999995,-77.0276294"
+ function fetchLocation(){
+      let geoLocationUrl = `http://open.mapquestapi.com/geocoding/v1/reverse?key=${key}&location=${myLati},${myLong}`
+  fetch(geoLocationUrl)
+  .then(respond => respond.json())
+  .then(data => {
+    console.log(data)
+      let street = data.results[0].locations[0].street;
+      let city = data.results[0].locations[0].adminArea5;
+      let state = data.results[0].locations[0].adminArea3;
+      mylocation = document.getElementById('mylocation');
+      mylocation.innerHTML = `Your are at ${street}, ${city}, ${state}`;
+      
+  })
+ }
+
+
+
+
+function loginAccess(){
+    let url = 'https://shrouded-badlands-76458.herokuapp.com/api/login';
+    fetch(url , {
+        method: 'post',
+        headers: { "content-type": "application/json", },
+        body : JSON.stringify({
+            'username' : "mwp",
+            'password' : "123"
+        })
+    })
+    .then((respond) => respond.json())
+    .then(data => {
+        
+    })
+}
