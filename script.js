748c5e7bc7690a20dfacde9e453d0cd003ab14eb
bisrat2020 <wow.bisrat11@gmail.com>
2021-06-08 20:50:01 -0400

    displaying the animation

--- script.js
+++ script.js
@@ -17,7 +17,7 @@
 // document.body.append(passInput)
 
 // let br2 = document.createElement('p');
-// document.body.append(br2); 
+// document.body.append(br2);
 // let btn =  document.createElement("BUTTON");
 // btn.innerHTML = "Login";
 // document.body.append(btn)
@@ -25,7 +25,7 @@
 // let div = document.querySelector("#outlet");
 // div.appendChild(text1,userLabel)
 
-const loginPage =`
+const loginPage = `
 <div id="loginDiv"><h3>Please Login</h3><hr><br>
 <label for="user">Username: </label>
 <input id="user" type="text"><br><br>
@@ -33,100 +33,105 @@ const loginPage =`
 <input id="pass" type="text"><br><br>
 <button id="loginBtn">Login</button></div>`;
 
-const animationPage =`<div id="anamtion">
+const animationPage = `<div id="anamtion">
 <span ><strong id="mylocation"></strong></span><br><hr>
 <textarea name="" id="display" cols="35" rows="15"></textarea><br><br>
 <button id="refresh">Refresh Animation</button>
 <button id="logout">LogOut</button>
-</div>`
+</div>`;
 
-const key = `kgwCfjylCzg0GhWKC2aaaMcLIc10aIQO`
-
+const key = `kgwCfjylCzg0GhWKC2aaaMcLIc10aIQO`;
+let playAnimation, animationString;
+let myToken;
 let mainDiv = document.getElementById("outlet");
-     mainDiv.innerHTML = loginPage;
-let loginDiv = document.getElementById('loginDiv');
-loginDiv.classList.add('area');
- 
+mainDiv.innerHTML = loginPage;
+let loginDiv = document.getElementById("loginDiv");
+loginDiv.classList.add("area");
 
+let loginBtn = document.getElementById("loginBtn");
+loginBtn.addEventListener("click", () => {
+  console.log("clicked");
+  history.pushState("AnimationPage", "?animation");
+  mainDiv.innerHTML = animationPage;
+  userLocation();
+  loginAccess();
 
+  let animDiv = document.getElementById("anamtion");
+  animDiv.classList.add("area");
 
-let loginBtn = document.getElementById('loginBtn');
-loginBtn.addEventListener('click',()=>{
-    console.log('clicked')
-    history.pushState("AnimationPage","?animation");
-    mainDiv.innerHTML = animationPage;
-    userLocation();
-    loginAccess();
+  let display = document.getElementById("display");
 
-     let animDiv = document.getElementById("anamtion");
-      animDiv.classList.add('area');
-      
-      let display = document.getElementById('display');
+  let refreshBtn = document.getElementById("refresh");
+  
 
-      let refreshBtn = document.getElementById('refresh');
-      let logoutBtn = document.getElementById('logout')
-    
-      refreshBtn.addEventListener('click',
-      animation = function(){
-          fetch(`https://shrouded-badlands-76458.herokuapp.com/api/animation`,
-          {
-              method:'GET',
-              headers:{
-                "content-type": "application/text",
-                Authorization: `Bearer ${myUniqueToken}`
-              }
-          })
-          .then((respond) => respond.text())
-          .then(data =>{
-              console.log(data)
-          })
+  refreshBtn.addEventListener(
+    "click",
+    (animation = function () {
+      fetch(`https://shrouded-badlands-76458.herokuapp.com/api/animation`, {
+        method: "GET",
+        headers: {
+          "content-type": "application/text",
+          Authorization: `Bearer ${myToken}`,
+        },
       })
+        .then((respond) => respond.text())
+        .then((data) => {
+          console.log(data);
+          animationString = data.split("=====\n");
+          if (playAnimation) {
+            clearInterval(playAnimation);
+          }
+          let start = 0;
+          playAnimation = setInterval(() => {
+            display.innerHTML = animationString[start];
+            start++;
+            if (start === animationString.length) {
+              start = 0;
+            }
+          }, 300);
+        });
+    })
+  );
+});
 
-      
 
-      
-})
+
+
 let mylocation;
-let myLong,myLati;
- function userLocation(){
-     navigator.geolocation.getCurrentPosition(
-         function(thisPosition){
-             myLong = thisPosition.coords.longitude;
-             myLati = thisPosition.coords.latitude;
-             console.log(myLong)
-             let geoLocationUrl = `http://open.mapquestapi.com/geocoding/v1/reverse?key=${key}&location=${myLati},${myLong}`
-             fetch(geoLocationUrl)
-             .then(respond => respond.json())
-             .then(data => {
-               console.log(data)
-                 let street = data.results[0].locations[0].street;
-                 let city = data.results[0].locations[0].adminArea5;
-                 let state = data.results[0].locations[0].adminArea3;
-                 mylocation = document.getElementById('mylocation');
-                 mylocation.innerHTML = `You are at ${street}, ${city}, ${state}`;
-                 
-             })
-         } 
-     )
- }
+let myLong, myLati;
+function userLocation() {
+  navigator.geolocation.getCurrentPosition(function (thisPosition) {
+    myLong = thisPosition.coords.longitude;
+    myLati = thisPosition.coords.latitude;
+    console.log(myLong);
+    let geoLocationUrl = `http://open.mapquestapi.com/geocoding/v1/reverse?key=${key}&location=${myLati},${myLong}`;
+    fetch(geoLocationUrl)
+      .then((respond) => respond.json())
+      .then((data) => {
+        console.log(data);
+        let street = data.results[0].locations[0].street;
+        let city = data.results[0].locations[0].adminArea5;
+        let state = data.results[0].locations[0].adminArea3;
+        mylocation = document.getElementById("mylocation");
+        mylocation.innerHTML = `You are at ${street}, ${city}, ${state}`;
+      });
+  });
+}
 //  "http://open.mapquestapi.com/geocoding/v1/reverse?key=kgwCfjylCzg0GhWKC2aaaMcLIc10aIQO&location=38.987779499999995,-77.0276294"
 
-
-
-
-let myToken;
-function loginAccess(){
-    let url = 'https://shrouded-badlands-76458.herokuapp.com/api/login';
-    fetch(url , {
-        method: 'post',
-        headers: { "content-type": "application/json", },
-        body : JSON.stringify({
-            'username' : "mwp",
-            'password' : "123456"
-        })
-    })
+function loginAccess() {
+  let url = "https://shrouded-badlands-76458.herokuapp.com/api/login";
+  fetch(url, {
+    method: "post",
+    headers: { "content-type": "application/json" },
+    body: JSON.stringify({
+      username: "mwp",
+      password: "123456",
+    }),
+  })
     .then((respond) => respond.json())
-    .then(data => {
-        myToken = data.token;
-    })
+    .then((data) => {
+      myToken = data.token;
+      animation();
+    });
 }
