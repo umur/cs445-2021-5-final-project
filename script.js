860cd63a38fc84f8c90301639e0425374d23728f
Luwam19 <79125146+Luwam19@users.noreply.github.com>
2021-06-10 21:51:30 -0500

    js file updated

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 // your code 
+window.onload = function windcoll(){
+const loginTemplate =
+        
+        <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">         
+        <div class="container">
+        <div id="form">
+              <div class="form-group">
+                <h1>Please Login</h1>
+                    <label>Username:</label>
+                    <input type="text" name="username" id="userid" value="mwp">
+                </div>
+                <div class="form-group">
+                    <label>Password:</label>
+                    <input type="text" id="password" placeholder="123" value="123456">
+                </div>
+                <div class="form-group">
+                    <button class="btn btn-success" id = "login">login</button>
+                </div>
+        </div>
+}
