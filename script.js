f0da854c4ae72c5b252e4416d1985b77b0ad2859
asgdomhail@gmail.com <63181283+Asget-dev@users.noreply.github.com>
2021-06-11 18:16:58 -0700

    updated file

--- script.js
+++ script.js
@@ -1,89 +1,79 @@
 // your code
 let myToken;
-let holder,play;
-let output = `  <div class="firstPages">    <samp id="location"></samp><br>
-                    <textarea  id="display" cols="30" rows="10"></textarea><br>
-                    <button id="refresh">Refresh Animation</button>
-                    <button id="logout">Logout</button>
-</div>`
+let holder, play;
+let output = 
+`<div class="firstPages">
+    <samp id="location"></samp><br>
+    <textarea  id="display" cols="30" rows="10"></textarea><br>
+    <button id="refresh">Refresh Animation</button>
+    <button id="logout">Logout</button>
+</div>`;
 
-let login =`<div class="firstPage">
-  User name<input type ="text"><br>
-  Password<Input type ="text"><br>
-  <button id = "submit">login</button>
-  </div>`
+let login = 
+`<div class="firstPage">
+    User name<input type ="text"><br>
+    Password<Input type ="text"><br>
+            <button id = "submit">login</button>
+</div>`;
 
-  const element22 = document.querySelector("#outlet");
-  element22.innerHTML = login;
-
+const element22 = document.querySelector("#outlet");
+element22.innerHTML = login;
 
 let aPage = document.querySelector(".firstPage");
 aPage.innerHTML;
 aPage.style.border = "solid";
 aPage.style.margin = "40%";
 aPage.style.marginTop = "50px";
-aPage.style.padding= "50px";
-// let aPages = document.querySelector(".firstPages");
-// aPages.innerHTML;
-// aPages.style.border = "solid";
-// aPages.style.margin = "40%";
-// aPages.style.marginTop = "50px";
-// aPages.style.padding= "50px";
+aPage.style.padding = "50px";
 
+
+  // -------------Animation page----------------//
 const longinbtn = document.querySelector("#submit");
-
-
-longinbtn.addEventListener("click", function loginfunction () {
-  history.pushState({ page: 1 }, "title 1", "?page=1"); //browther ignore title
-  
-
+longinbtn.addEventListener("click", function loginfunction() {
+history.pushState({ page: 1 }, "title 1", "?page=1"); 
   const y = document.querySelector("#outlet");
   y.innerHTML = output;
-
   myLocation();
   getAutentication();
- 
   const refreshButton = document.querySelector("#refresh");
   const display = document.querySelector("#display");
   const logout = document.querySelector("#logout");
 
-
-  
-  refreshButton.addEventListener('click',(animations = function (){
-
-    fetch(`https://shrouded-badlands-76458.herokuapp.com/api/animation`,{
-      method: "GET",
-      headers: {
-        "Content-Type": "application/text",
-        Authorization: `Bearer ${myToken}`,
-    }
-    })
-      .then((response)=>response.text())
-      .then((data)=>{
-          holder=data.split('=====\n')
-          if(play){
-            clearInterval(play)
-          }    
-       let animationStart = 0;
-       play=setInterval(()=>{
-          display.innerHTML = holder[animationStart];
-          animationStart++;
-          if(animationStart==holder.length){
-            animationStart = 0;
-          }
-       },300)
+  refreshButton.addEventListener(
+    "click",
+    (animations = function () {
+      fetch(`https://shrouded-badlands-76458.herokuapp.com/api/animation`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/text",
+          Authorization: `Bearer ${myToken}`,
+        },
       })
-  })
-  )
-  logout.addEventListener('click',()=>{
+        .then((response) => response.text())
+        .then((data) => {
+          holder = data.split("=====\n");
+          if (play) {
+            clearInterval(play);
+          }
+          let animationStart = 0;
+          play = setInterval(() => {
+            display.innerHTML = holder[animationStart];
+            animationStart++;
+            if (animationStart == holder.length) {
+              animationStart = 0;
+            }
+          }, 300);
+        });
+    })
+  );
+  logout.addEventListener("click", () => {
     history.pushState("logout", "?logout");
     element22.innerHTML = login;
     const longinbtn2 = document.querySelector("#submit");
-    longinbtn2.addEventListener('click',loginfunction);
+    longinbtn2.addEventListener("click", loginfunction);
     clearInterval(play);
-  })
-}
-);
+  });
+});
 
 // -------------Geolocation----------------//
 
@@ -105,25 +95,26 @@ function myLocation() {
         let loc = document.querySelector("#location");
         loc.innerHTML = `${city},${state},${country}`;
       });
-    } 
+  }
 }
 
+// -------------login Page----------------//
 function getAutentication() {
   // e.preventDefault();
   let fn = {
     method: "POST",
     headers: {
-      "content-type": "application/json"
+      "content-type": "application/json",
     },
     body: JSON.stringify({
       username: "mwp",
-      password:"123456"
-    })
+      password: "123456",
+    }),
   };
-  fetch("https://shrouded-badlands-76458.herokuapp.com/api/login", fn)// for test since we do not have api
-  .then((res) =>res.json())
-  .then(data=>{
-      myToken = data.token
+  fetch("https://shrouded-badlands-76458.herokuapp.com/api/login", fn) // for test since we do not have api
+    .then((res) => res.json())
+    .then((data) => {
+      myToken = data.token;
       animations();
-  });
+    });
 }
