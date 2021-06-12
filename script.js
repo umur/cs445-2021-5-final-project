aaad24450c6380f75120ab36a3c3d5f4433eb1d3
asgdomhail@gmail.com <63181283+Asget-dev@users.noreply.github.com>
2021-06-11 18:12:45 -0700

    updated animation function

--- script.js
+++ script.js
@@ -1,75 +1,39 @@
 // your code
 let myToken;
 let holder,play;
-let output = `  <div id="firstPage">    <samp id="location"></samp><br>
+let output = `  <div class="firstPages">    <samp id="location"></samp><br>
                     <textarea  id="display" cols="30" rows="10"></textarea><br>
                     <button id="refresh">Refresh Animation</button>
                     <button id="logout">Logout</button>
 </div>`
-let aPage = document.querySelector("div");
+
+let login =`<div class="firstPage">
+  User name<input type ="text"><br>
+  Password<Input type ="text"><br>
+  <button id = "submit">login</button>
+  </div>`
+
+  const element22 = document.querySelector("#outlet");
+  element22.innerHTML = login;
+
+
+let aPage = document.querySelector(".firstPage");
+aPage.innerHTML;
 aPage.style.border = "solid";
+aPage.style.margin = "40%";
+aPage.style.marginTop = "50px";
+aPage.style.padding= "50px";
+// let aPages = document.querySelector(".firstPages");
+// aPages.innerHTML;
+// aPages.style.border = "solid";
+// aPages.style.margin = "40%";
+// aPages.style.marginTop = "50px";
+// aPages.style.padding= "50px";
 
-let tag2 = document.createElement("form");
-let text22 = document.createTextNode("Password");
-tag2.appendChild(text22);
-tag2.id = "form";
-let element22 = document.querySelector("#outlet");
-element22.appendChild(tag2);
-
-// let tag1 = document.createElement("p");
-// let text2 = document.createTextNode("Password");
-// tag1.appendChild(text2);
-// let element2 = document.querySelector("#form");
-// element2.appendChild(tag1);
-
-let user = document.createElement("INPUT");
-user.setAttribute("type", "text");
-// user.setAttribute("value", "username!");
-user.id = "users";
-document.body.appendChild(user);
-let userElement = document.querySelector("#form");
-userElement.appendChild(user)
-
-let password = document.createElement("INPUT");
-password.setAttribute("type", "text");
-// password.setAttribute("value", "username");
-password.id = "pswd";
-document.body.appendChild(password);
-let passwordElement = document.querySelector("#form");
-passwordElement.appendChild(password);
-
-let login = document.createElement("button");
-let text = document.createTextNode("Login");
-login.appendChild(text);
-login.id = "submit";
-let loginElement = document.querySelector("#form");
-loginElement.appendChild(login);
-
-// const form = document.querySelector("form");
-// form.addEventListener("submit", postData);
-
-// function postData(e) {
-//   e.preventDefault();
-//   const users = document.querySelector("#users").value;
-//   const pswd = document.querySelector("#pswd").value;
-//   const config = {
-//     method: "POST",
-//     body: JSON.stringify({ users: "1", pswd: 3 }),
-//     headers: {
-//       "Content-Type": "application/json",
-//     },
-//   };
-//   fetch("https://jsonplaceholder.typicode.com/comments", config)// for test since we do not have api
-//   .then((res) =>res.json())
-//   .then(data=>{
-//       for(i of data){
-//           console.log(i);
-//       }
-//   });
-// }
 const longinbtn = document.querySelector("#submit");
 
-longinbtn.addEventListener("click", (_) => {
+
+longinbtn.addEventListener("click", function loginfunction () {
   history.pushState({ page: 1 }, "title 1", "?page=1"); //browther ignore title
   
 
@@ -86,14 +50,7 @@ longinbtn.addEventListener("click", (_) => {
 
   
   refreshButton.addEventListener('click',(animations = function (){
-    // let url2 = ;
-    // const config2 = {
-    //   method: "GET",
-    //   headers: {
-    //     "Content-Type": "application/text",
-    //     Authorization: `Bearer ${myToken}`,
-    //   }
-    // };
+
     fetch(`https://shrouded-badlands-76458.herokuapp.com/api/animation`,{
       method: "GET",
       headers: {
@@ -120,14 +77,13 @@ longinbtn.addEventListener("click", (_) => {
   )
   logout.addEventListener('click',()=>{
     history.pushState("logout", "?logout");
-    // element22.innerHTML = logout 
+    element22.innerHTML = login;
+    const longinbtn2 = document.querySelector("#submit");
+    longinbtn2.addEventListener('click',loginfunction);
+    clearInterval(play);
   })
 }
 );
-
-// window.addEventListener("popstate", function (event) {
-//   console.log("state: " + JSON.stringify(event.state));
-// });
 
 // -------------Geolocation----------------//
 
@@ -151,9 +107,6 @@ function myLocation() {
       });
     } 
 }
-
-
-
 
 function getAutentication() {
   // e.preventDefault();
